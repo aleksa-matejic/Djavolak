3ff6e5f5c1509e66e98c9d2f035967c908f9d884
Aleksa Matejic <aleksa.matejic@endava.net>
2017-03-28 16:17:28 +0200

    Minor text fixes, uncommenting pref store for options

--- app/src/main/java/com/aleksa/matejic/app/OptionsActivity.java
+++ app/src/main/java/com/aleksa/matejic/app/OptionsActivity.java
@@ -9,8 +9,8 @@ import com.aleksa.matejic.app.utils.SharedPreferencesStore;
 
 public class OptionsActivity extends Activity
 {
-    // CheckBox cbSounds;
-    // CheckBox cbVibration;
+    CheckBox cbSounds;
+    CheckBox cbVibration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,30 +18,30 @@ public class OptionsActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        // cbSounds = (CheckBox) findViewById(R.id.cbSounds);
-        // cbVibration = (CheckBox) findViewById(R.id.cbVibration);
+        cbSounds = (CheckBox) findViewById(R.id.cbSounds);
+        cbVibration = (CheckBox) findViewById(R.id.cbVibration);
 
-        // boolean checked = SharedPreferencesStore.getInstance(OptionsActivity.this).readBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).SOUNDS);
-        // cbSounds.setChecked(checked);
-        // checked = SharedPreferencesStore.getInstance(this).readBoolean(SharedPreferencesStore.getInstance(this).VIBRATION);
-        // cbVibration.setChecked(checked);
+        boolean checked = SharedPreferencesStore.getInstance(OptionsActivity.this).readBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).SOUNDS);
+        cbSounds.setChecked(checked);
+        checked = SharedPreferencesStore.getInstance(this).readBoolean(SharedPreferencesStore.getInstance(this).VIBRATION);
+        cbVibration.setChecked(checked);
 
-        // cbSounds.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        // {
-        //     @Override
-        //     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-        //     {
-        //         SharedPreferencesStore.getInstance(OptionsActivity.this).saveBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).SOUNDS, isChecked);
-        //     }
-        // });
+        cbSounds.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                SharedPreferencesStore.getInstance(OptionsActivity.this).saveBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).SOUNDS, isChecked);
+            }
+        });
 
-        // cbVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        // {
-        //     @Override
-        //     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-        //     {
-        //         SharedPreferencesStore.getInstance(OptionsActivity.this).saveBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).VIBRATION, isChecked);
-        //     }
-        // });
+        cbVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                SharedPreferencesStore.getInstance(OptionsActivity.this).saveBoolean(SharedPreferencesStore.getInstance(OptionsActivity.this).VIBRATION, isChecked);
+            }
+        });
     }
 }
